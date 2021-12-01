c0b312283ec4b5c2ebdb0154c5d3e158fae7038c
Clement Dumas <dumasclement2002@gmail.com>
2021-12-01 16:40:22 +0100

    solved day 1

--- Butanium_aoc/day1.py
+++ Butanium_aoc/day1.py
@@ -1,11 +1,18 @@
+file = open("puzzle_input/day1.txt", 'r')
+lines = list(map(int, file.readlines()))
 
-file = open("puzzle_input/day1.txt",'r')
-lines = file.readlines()
-acc = int(lines[0])
-r = 0
-for i in lines:
-    if int(i) > acc:
-        r += 1
-    acc = int(i)
 
-print(r)
+def solve(measures):
+    acc = measures[0]
+    r = 0
+    for i in measures:
+        if i > acc:
+            r += 1
+        acc = i
+    return r
+
+
+print("result for part 1 : %d" % solve(lines))
+
+measurements = list(sum(lines[i - 1:i + 2]) for i in range(1, len(lines) - 1))
+print("result for part 2 : %d" % solve(measurements))
