a3a325bde82d8076a4d018265431a49e4505c89b
martin <supermartruc@gmail.com>
2021-12-06 21:43:57 +0100

    fixed typo

--- supermartruc_AOC/AOC_py_4.py
+++ supermartruc_AOC/AOC_py_4.py
@@ -7,15 +7,16 @@ print(boards)
 check = lambda board: (sum([[nb[-1] for nb in row] == 5 * ['*'] for row in board]) + sum(
     [[row[k][-1] for row in board] == 5 * ['*'] for k in range(5)])) > 0
 sunmark = lambda board: sum([sum([int(nb) for nb in row if nb[-1] != '*']) for row in board])
-mark = lambda nbd, boards: [[[nb + ((nbd == nb) - (nb[-1] == '*')) * '*' for nb in row] for row in board] for board in
+mark = lambda nbd, boards: [[[nb + (nbd == nb) * '*' for nb in row] for row in board] for board in
                             boards]
 
 n = 0
+""" PART 1 """
+while [check(b) for b in boards]==len(boards)*[False]: boards=mark(drawn[n],boards); n+=1
+print(int(drawn[n-1])*sunmark(boards[[check(b) for b in boards].index(True)]))
 
-"""while [check(b) for b in boards]==len(boards)*[False]: boards=mark(drawn[n],boards); n+=1
-print(int(drawn[n-1])*sunmark(boards[[check(b) for b in boards].index(True)]))"""  # PART 1
-
-"""while sum([check(b) for b in boards])!=len(boards)-1: boards=mark(drawn[n],boards); n+=1 #PART 2
-x=[boards[[check(b) for b in boards].index(False)]]
-while not check(x[0]): x=mark(drawn[n],x); n+=1;
-print(int(drawn[n-1])*sunmark(x[0]))"""
+""" PART 2 """
+while sum([check(b) for b in boards]) != len(boards) - 1: boards = mark(drawn[n], boards); n += 1  # PART 2
+x = [boards[[check(b) for b in boards].index(False)]]
+while not check(x[0]): x = mark(drawn[n], x); n += 1;
+print(int(drawn[n - 1]) * sunmark(x[0]))
