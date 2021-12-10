58bf6b1904c1edaa4b7ca0f8b49074fa5b034b51
infrandomness <infrandomness@gmail.com>
2021-12-10 20:32:58 +0100

    Add files

---
+++ infrandomness_aoc/AoC/Days/SonarSweep.cs
@@ -0,0 +1,66 @@
+﻿using AoC.Utils;
+
+namespace AoC.Days;
+
+public class SonarSweep : IDay
+{
+
+    private static int ProcessIncremental(List<int> numbers)
+    {
+        int? number = null;
+        var count = 0;
+        foreach (var t in numbers)
+        {
+            if (number < t)
+            {
+                count++;
+            }
+
+            number = t;
+        }
+
+        return count;
+    }
+
+    private static int ProcessIncrementalPartTwo(List<int> numbers)
+    {
+        int? number = null;
+        var count = 0;
+        
+        foreach (var t in numbers)
+        {
+            for (var i = 1; i < numbers.Count; i++)
+            {
+                for (var j = 2; j < numbers.Count; j++)
+                {
+                    if (number < t + numbers[i] + numbers[j])
+                    {
+                        count++;
+                    }
+
+                    number = t + numbers[i] + numbers[j];
+                }
+
+            }
+        }
+        return count;
+    }
+    private static void SolvePartOne(List<int> content)
+    {
+        Console.WriteLine("=== Part One ===");
+        Console.WriteLine("Number of increments : {0}", ProcessIncremental(content));
+    }
+
+    private static void SolvePartTwo(List<int> content)
+    {
+        Console.WriteLine("=== Part Two ===");
+        Console.WriteLine("Number of increments : {0}", ProcessIncrementalPartTwo(content));
+    }
+
+    public void Run(string[] arguments)
+    {
+        var content = Io.ReadFileIntoIntList("day1");
+        SolvePartOne(content);
+        SolvePartTwo(content);
+    }
+}
---
+++ infrandomness_aoc/AoC/Utils/Aoc.cs
@@ -0,0 +1,20 @@
+﻿using AoC.Days;
+
+namespace AoC.Utils;
+
+public class Aoc
+{
+    public static readonly string? InputPath = System.Environment.GetEnvironmentVariable("AOC_INPUT_PATH");
+
+    //TODO: Create a Day? type instead of returning null to let the parent function implement its own error handling mechanism
+    public static IDay? GetDay(string day)
+    {
+        switch (day)
+        {
+            case "1":
+                return new SonarSweep();
+        }
+
+        return null;
+    }
+}
